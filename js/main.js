e34dc887b272b487cdb93e16d945062fae6fd100
Michele <micheleierniero@gmail.com>
2021-09-02 18:14:15 +0200

    icone colorate

--- js/main.js
+++ js/main.js
@@ -97,20 +97,30 @@ const icons = [
     },
 ];
 
+const colors = [
+  "blue",
+  "orange",
+  "purple"
+];
+  
+
+
 const itemContainer = document.getElementById('item_container');
 const wrap = document.getElementById('wrap');
 // console.log(itemContainer);
 
+const coloredArray = colorIcons(icons, colors);
 
+// PRIMA PARTE
 // stampa icone in html
 const print = (array, container) => {
   container.innerHTML = '';
 
   array.forEach((element) => {
-    const { name, family, prefix, type } = element
+    const { name, family, prefix, color } = element
     wrap.innerHTML += `
     <div id="item_container">
-        <i class="${family} ${prefix + name}"></i>
+        <i class="${family} ${prefix + name}" style="color: ${color}"></i>
             <p>
                 ${name.toUpperCase()}
             </p>
@@ -119,8 +129,33 @@ const print = (array, container) => {
 });
 };
 
-print(icons, wrap);
+print(coloredArray, wrap)
+
 // / stampa icone in html
+// / PRIMA PARTE
 
 
+// SECONDA PARTE
+function colorIcons(array, colors) {
+  const types = getTypes(array);
+  // console.log(types);
+  const coloredArray = array.map((element) => {
+    const indexType = types.indexOf(element.type);
+    console.log(indexType);
+    element.color = colors[indexType];
+    return element;
+  })
 
+  return coloredArray;
+};
+
+function getTypes(array) {
+  const types = [];
+  array.forEach((element) => {
+    console.log(element.type)
+    if (!types.includes(element.type))
+    types.push(element.type)
+  })
+  return types
+};
+// / SECONDA PARTE
